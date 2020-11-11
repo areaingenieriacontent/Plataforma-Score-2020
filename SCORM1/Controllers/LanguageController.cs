a2ac66d2280ee1514f6f6a372f72cd6903b81615
Deyvid Martinez <deyvid.martinez@hotmail.com>
2020-11-11 09:45:10 -0500

    Traduccion Bureau, Creacion de recursos, archivos .resx y controladores

---
+++ SCORM1/Controllers/LanguageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCORM1.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index(string language)
+        {
+
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+
+            return View();
+        }
+    }
+}
